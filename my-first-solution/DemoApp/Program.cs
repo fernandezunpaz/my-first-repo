887d634abf8d998cb1dcb211ad9d9b99318d7d78
Daniel Alejandro Fernandez <daf111@yahoo.com.ar>
2021-03-25 19:57:24 -0300

    Update Program.cs

--- my-first-solution/DemoApp/Program.cs
+++ my-first-solution/DemoApp/Program.cs
@@ -10,7 +10,11 @@ namespace DemoApp
     {
         static void Main(string[] args)
         {
+<<<<<<< Updated upstream
             Console.WriteLine("Ingrese su nickname:");
+=======
+            Console.WriteLine("Enter your nickname: ");
+>>>>>>> Stashed changes
             string nombre = Console.ReadLine();
             Console.WriteLine("Hola {0}", nombre);
         }
