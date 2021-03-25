fb0ac80f21b5af67fbb7b70fa068c064357e991f
Daniel Alejandro Fernandez <daf111@yahoo.com.ar>
2021-03-25 19:10:06 -0300

    Revert "Change message language"
    This reverts commit 7f6dd16866fb018ee3db7171e8fd165631a33f59.

--- my-first-solution/DemoApp/Program.cs
+++ my-first-solution/DemoApp/Program.cs
@@ -10,7 +10,7 @@ namespace DemoApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola mundo!");
         }
     }
 }
